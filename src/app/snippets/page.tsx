89672e1d355317863324312c542a29d201365f00
UTKARSH SINGH <utkarshsingh132002@Gmail.com>
2024-10-27 16:48:52 +0530

    pages for website

---
+++ src/app/snippets/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Snippets = () => {
+  return (
+    <div>
+      Hii This is a snippet
+    </div>
+  )
+}
+
+export default Snippets
---
+++ src/app/uploadSnippets/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UploadSnippets = () => {
+  return (
+    <div>
+      Here we can upload snippets
+    </div>
+  )
+}
+
+export default UploadSnippets

e79bbfd825e2453b9c0559c318e7563d4986fe4d
UTKARSH SINGH <utkarshsingh132002@Gmail.com>
2024-11-02 13:01:47 +0530

    User chat loading and no messages

--- src/app/userChat/page.tsx
+++ src/app/userChat/page.tsx
@@ -4,6 +4,7 @@ import { useToast } from "@/hooks/use-toast";
 import { GetUserChat } from "../../../actions/GetUserChat";
 import ChatCard from "./_components/ChatCard";
 import { AnimatePresence } from "framer-motion";
+import { Loader2, MessageCircleOff } from "lucide-react";
 
 interface ChatTypes {
   id: string;
@@ -20,11 +21,13 @@ interface ChatTypes {
 const UserChat = () => {
   const [userChat, setUserChat] = useState<ChatTypes[] | null>(null);
   const [loadingDelete, setLoadingDelete] = useState(false);
+  const [loading, setLoading] = useState(false);
   const { toast } = useToast();
 
   useEffect(() => {
     const getUserChat = async () => {
       try {
+        setLoading(true);
         const res = await GetUserChat();
         if (res.status === false) {
           throw new Error(res.msg);
@@ -50,30 +53,45 @@ const UserChat = () => {
         toast({
           description: error.message || "Something went wrong",
         });
+      } finally {
+        setLoading(false);
       }
     };
     getUserChat();
   }, [loadingDelete]);
 
+
+  if(loading){
+    return <div className="w-screen pt-44 overflow-hidden flex justify-center items-center">
+      <Loader2 className="animate-spin w-7 h-7"/>
+    </div>
+  }
+
   return (
     <div className="mt-16 xl:pl-20  ml-2 pr-3 xl:pr-0 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-      {userChat
-        ?.sort((a: { createdAt: any }, b: { createdAt: any }) => {
-          return (
-            new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
-          );
-        })
-        .map((chat) => (
-          <ChatCard
-            key={chat.id}
-            id={chat.id}
-            text={chat.text}
-            username={chat.sender.username}
-            createdAt={chat.createdAt}
-            profileImage={chat.sender.profileImage}
-            setLoadingDelete={setLoadingDelete}
-          />
-        ))}
+      {userChat && userChat.length > 0 ? (
+        userChat
+          ?.sort((a: { createdAt: any }, b: { createdAt: any }) => {
+            return (
+              new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
+            );
+          })
+          .map((chat) => (
+            <ChatCard
+              key={chat.id}
+              id={chat.id}
+              text={chat.text}
+              username={chat.sender.username}
+              createdAt={chat.createdAt}
+              profileImage={chat.sender.profileImage}
+              setLoadingDelete={setLoadingDelete}
+            />
+          ))
+      ) : (
+        <div className="flex justify-center items-center text-3xl font-bold bg-opacity-30 opacity-60 gap-x-3">
+          No Messages <MessageCircleOff />
+        </div>
+      )}
     </div>
   );
 };

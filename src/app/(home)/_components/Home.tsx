47269480b0e6bb6312bc9da0eefa2cd18d1c5d4b
UTKARSH SINGH <utkarshsingh132002@Gmail.com>
2024-12-26 15:10:53 +0530

    feeds added at home

--- src/app/(home)/_components/Home.tsx
+++ src/app/(home)/_components/Home.tsx
@@ -7,11 +7,38 @@ import { CardHoverEffectDemo } from "../_components/HoverEffect";
 import Footer from "../_components/Footer";
 import { GetAllSnippets } from "../../../../actions/GetAllSnippets";
 import { SnippetType } from "@/app/snippets/page";
+import { motion } from "framer-motion";
+import ImageCard, {
+  ImageCardProps,
+} from "@/app/feeds/_components/HeaderImageCard";
+import { DeleteFeed } from "../../../../actions/DeleteFeed";
+import { GetFeedByProfile } from "../../../../actions/GetfeedByProfile";
+import { GetLatestFeed } from "../../../../actions/GetLatestFeed";
 
 const HomeSection = () => {
+  const [cardData, setCardData] = useState<any | null>(null);
   const [loading, setLoading] = useState(false);
   const [snippets, setSnippets] = useState<SnippetType[] | null>(null);
-  
+
+  const fetchFeedData = async () => {
+    try {
+      const res = await GetLatestFeed();
+      if (res.status === false) {
+        throw new Error(res.msg);
+      }
+      // console.log(res);
+      setCardData(res?.feedsData);
+    } catch (error) {
+      // console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchFeedData();
+  }, []);
+
   useEffect(() => {
     const getAllSnippets = async () => {
       setLoading(true);
@@ -25,10 +52,43 @@ const HomeSection = () => {
     getAllSnippets();
   }, []);
 
+  const handleDeleteFeed = async (feedId: string) => {
+    try {
+      setCardData((prevData: any) =>
+        prevData ? prevData.filter((feed: any) => feed.id !== feedId) : prevData
+      );
+      const res = await DeleteFeed(feedId);
+      if (res.status === false) {
+        throw new Error(res.msg);
+      }
+    } catch (error) {
+      // console.log(error);
+    }
+  };
+
   return (
     <>
-      <Header/>
+      <Header />
       <div className="grid grid-cols-1  sm:grid-cols-2  lg:grid-cols-3 gap-x-6 gap-y-14  mt-10 xl:ml-20 xl:mr-20 ml-3 mr-3">
+        {cardData
+          ?.slice(2, 5)
+          .map((cardDataItems: ImageCardProps, index: number) => (
+            <motion.div
+              key={index}
+              className="max-w-md w-full min-h-full flex flex-col justify-between rounded-xl overflow-hidden"
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.9 }}
+            >
+              <ImageCard
+                {...cardDataItems}
+                handleDeleteFeed={handleDeleteFeed}
+              />
+            </motion.div>
+          ))}
+      </div>
+      <HeroScrollDemo />
+      <div className="grid grid-cols-1  sm:grid-cols-2  lg:grid-cols-3 gap-x-6 gap-y-14  mb-24 xl:ml-20 xl:mr-20 ml-3 mr-3 -mt-64 md:-mt-0">
         {snippets
           ?.sort(
             (a: any, b: any) =>
@@ -41,9 +101,8 @@ const HomeSection = () => {
             </div>
           ))}
       </div>
-      <HeroScrollDemo/>
-      <CardHoverEffectDemo/>
-      <Footer/>
+      <CardHoverEffectDemo />
+      <Footer />
     </>
   );
 };
